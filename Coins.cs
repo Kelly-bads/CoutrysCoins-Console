764da93afabc79bd218870eb2d5ddec69f4adbb7
Kelly Barbosa <kyba@gft.com>
2022-03-03 15:18:26 -0300

    versão 0.0.1

---
+++ Coins.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Coins
+{
+    class Coins
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal vlr = 10562.25m;
+            Console.WriteLine(
+                vlr.ToString(
+                    "N", 
+                    CultureInfo.CreateSpecificCulture("pt-BR")
+                )   
+            );
+        }
+    }
+}
